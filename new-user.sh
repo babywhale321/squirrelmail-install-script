4b60700163a946167c013ceba1a1d0351cff9de3
babywhale321 <107947235+babywhale321@users.noreply.github.com>
2022-08-26 02:44:27 +0000

    Update new-user.sh

--- new-user.sh
+++ new-user.sh
@@ -6,4 +6,4 @@ passwd $uservar
 
 #new user defined variables creation and permissions
 mkdir /var/www/html/$uservar ; usermod -m -d /var/www/html/$uservar $uservar
-chown -R $uservar:$uservar /var/www/html/$uservar ; chmod 777 -R /var/www/html/$uservar
+chown -R $uservar:$uservar /var/www/html/$uservar
