b35cd036a98e342204ac476feba52de6a732c108
babywhale321 <107947235+babywhale321@users.noreply.github.com>
2022-09-19 02:26:20 +0000

    Update squirrelmail-install.sh
    Additions:
    - troubleshooting messages
    - condensed user input at start of script.
    - minor visual changes
    
    Removed:
    - Unused lines of code

--- squirrelmail-install.sh
+++ squirrelmail-install.sh
@@ -5,36 +5,35 @@
 echo "What is the public ipv4 address of your server: 123.123.123.123 = an example: "
 read -r ipvar
 
+clear
 echo "What is the registerd domain name? example.com = an example: "
 read -r hostvar
 
+clear
 #admin email
 echo "What will the server admin email be? admin@example.com = an example: "
 read -r adminvar
 
-#timezone selection
-echo "What is your continent? America = an example: " 
-read -r zone1var
+clear
+#new user for webpage login gui
+echo "What will be the new user for the sign in page? bill = an example: "
+read -r uservar
+useradd $uservar
+passwd $uservar
 
-echo "What is your city? New_York = an example: " 
-read -r zone2var
-
-echo ""
-echo "Please verify that everything is correct then press enter"
-echo "
-ipv4: $ipvar
+clear
+echo " 
+ipv4: $ipvar 
 Domain: $hostvar
-admin email: $adminvar
-Timezone: $zone1var $zone2var "
-read -r emptyvar
+admin email: $adminvar 
+Webpage GUI User: $uservar "
+echo " "
+read -p "Please verify that all options are correct then press enter:"
 
 #Required repo and applications to be updated and installed
-apt update ; apt upgrade -y ; apt-get install software-properties-common -y ; add-apt-repository ppa:ondrej/php -y ; add-apt-repository ppa:ondrej/apache2 -y ; apt install dialog -y ; apt install bsdutils -y || exit
-apt install -y subversion apache2 build-essential mariadb-server mariadb-client php postfix dovecot-imapd dovecot-pop3d || exit
-
-#set timezone on apache
-a2enmod rewrite expires ; sed -i "s/;date.timezone.*/date.timezone = $zone1var\/\$zone2var/" /etc/php/*/apache2/php.ini || exit
-systemctl start apache2 mariadb ; systemctl enable apache2 mariadb ; mysql_secure_installation || exit
+apt update ; apt upgrade -y ; apt-get install software-properties-common -y ; add-apt-repository ppa:ondrej/php -y ; add-apt-repository ppa:ondrej/apache2 -y ; apt install dialog bsdutils -y || echo "An error has accoured regarding the installation of 1 or more of these applications: software-properties-common, dialog, bsdutils" exit
+apt install -y subversion apache2 build-essential mariadb-server mariadb-client php postfix dovecot-imapd dovecot-pop3d || echo "An error has accoured regarding the installation of 1 or more of these applications: subversion, apache2, build-essential, mariadb-server, mariadb-client, php, postfix, dovecot-imapd, dovecot-pop3d" exit
+systemctl start apache2 mariadb ; systemctl enable apache2 mariadb ; mysql_secure_installation || echo "An error has accoured regarding starting 1 or more of these services: apache2, mariadb, mysql_secure_installation" exit
 
 #Download squirrelmail dev version
 svn checkout https://svn.code.sf.net/p/squirrelmail/code/trunk/squirrelmail
@@ -56,25 +55,19 @@ allow from all
 </Directory>
 ErrorLog /var/log/apache2/$hostvar-error_log
 CustomLog /var/log/apache2/$hostvar-access_log common
-</VirtualHost>" > /etc/apache2/sites-available/$hostvar.conf
+</VirtualHost>" > /etc/apache2/sites-available/$hostvar.conf || echo "An error has accoured with the config file in /etc/apache2/sites-available/$hostvar.conf" exit
 
-a2ensite $hostvar.conf ; a2dissite 000-default.conf  ; apache2ctl configtest ; echo "$ipvar $hostvar" >> /etc/hosts ; systemctl restart apache2 postfix dovecot || exit
+a2ensite $hostvar.conf ; a2dissite 000-default.conf ; echo "$ipvar $hostvar" >> /etc/hosts ; systemctl restart apache2 postfix dovecot || echo "An error has accoured regarding services and applications of 1 or more of these listed: apache2, postfix, dovecot, a2dissite, a2ensite" exit
 
 #dir creation and permision set
 mkdir -p /var/local/squirrelmail/data/ ; chown -R www-data:www-data /var/local/squirrelmail/data
 
-#new user for webpage login gui
-echo ""
-echo "What is the new user for sign in page on website? bill = an example: "
-read -r uservar
-useradd $uservar
-passwd $uservar
-
 #new user defined variables creation and permissions
-mkdir /var/www/html/$uservar ; usermod -m -d /var/www/html/$uservar $uservar ; chown -R $uservar:$uservar /var/www/html/$uservar
+mkdir /var/www/html/$uservar ; usermod -m -d /var/www/html/$uservar $uservar ; chown -R $uservar:$uservar /var/www/html/$uservar || echo "An error has accoured with the creation of $uservar dir and or permissions" exit 
 
 #moves the default config to active config
-cd /var/www/html ; svn update squirrelmail ; cd squirrelmail/config/ ; cp config_default.php config.php || exit
+cp /var/www/html/squirrelmail/config/config_default.php /var/www/html/squirrelmail/config/config.php
 
+echo "--------------------------------------------------------------------------------------------------"
 echo "Please test the web page to see if everything is working."
 echo "Make sure to Run: perl /var/www/html/squirrelmail/config/conf.pl to further configure squirrelmail"
